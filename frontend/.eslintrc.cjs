41f0714e5951512bfe1ca653dff8933fe1e68838
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2026-06-30 11:56:43 +0530

    fixed the lint error

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,92 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+  },
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+
+  plugins: ['@typescript-eslint', 'react-hooks', 'import'],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+
+    '@typescript-eslint/explicit-function-return-type': 'error',
+
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+
+    '@typescript-eslint/no-floating-promises': 'error',
+
+    '@typescript-eslint/no-misused-promises': 'error',
+
+    '@typescript-eslint/strict-boolean-expressions': 'error',
+
+    'react-hooks/rules-of-hooks': 'error',
+
+    'react-hooks/exhaustive-deps': 'warn',
+
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+
+    'import/no-cycle': 'error',
+
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+
+    eqeqeq: ['error', 'always'],
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+};
